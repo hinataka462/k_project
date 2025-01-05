c19d9346dd3937730456a3e24012d0c265e4ee9b
hinataka <ylingmu019@gmail.com>
2025-01-05 23:24:06 +0900

    Remove all files

--- webapp_02_sp04/src/main/java/com/example/webapp_02_sp04/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webapp_02_sp04.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.webapp_02_sp04.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
